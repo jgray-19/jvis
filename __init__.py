326a226536c39ff6b91727b73f21c82431165f1b
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2025-03-28 09:57:34 +0100

    Update and rename __init__.py to jvis/__init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""
-JOMCVIS: A Python package for visualising data for omc based projects.
-"""
-
-__version__ = "0.0.1"
---
+++ jvis/__init__.py
@@ -0,0 +1,5 @@
+"""
+JVIS: A Python package for visualising data to my preferences.
+"""
+
+__version__ = "0.0.1"
