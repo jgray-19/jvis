c96c9b65017de4cc1c9d87f44f0a0a403621cf1f
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2025-03-28 09:58:34 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jomcvis",
+    version="0.0.1",
+    author="Joshua Gray",
+    description="A package to add a lot of functionality using matplotlib",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+)
